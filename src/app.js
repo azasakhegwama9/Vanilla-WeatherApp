f70cfbb3da37f05c882967568f078ee57c6e3fc2
azasakhegwama9 <135711777+azasakhegwama9@users.noreply.github.com>
2023-06-16 13:43:02 +0200

    Added Weather Icons

---
+++ src/app.js
@@ -0,0 +1,53 @@
+function formatDate(timestamp) {
+    let date = new Date();
+    let hours = date.getHours();
+    if (hours < 10 ) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ];
+    let day = days[date.getDay()];
+    return `${day}   ${hours}:${minutes}`;
+}
+
+function displayTemp (response){
+    let city = document.querySelector("#city");
+    city.innerHTML = response.data.name;
+
+    let temperature = document.querySelector("#temp"); 
+    temperature.innerHTML = Math.round(response.data.main.temp);
+
+    let humid = document.querySelector("#humidity"); 
+    humid.innerHTML = `Humidity: ${response.data.main.humidity}%`;
+
+    let windSpeed = document.querySelector("#wind");
+    windSpeed.innerHTML = `Wind: ${Math.round(response.data.wind.speed)}km/h`;
+    
+    let pressure = document.querySelector("#pressure");
+    pressure.innerHTML = `Pressure: ${(response.data.main.pressure)}`;
+ 
+    let weather = document.querySelector("#weather-description");
+    weather.innerHTML = response.data.weather[0].description;
+
+    let date = document.querySelector("#date");
+    date.innerHTML = formatDate(response.data.dt * 1000);
+
+    let icon = document.querySelector("#icon1");
+    icon.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+}
+
+let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
+let city = "Port Elizabeth";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemp);   
