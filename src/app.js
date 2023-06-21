b20dd7af4e07a3fae196b964b51a8fc577d94fac
azasakhegwama9 <135711777+azasakhegwama9@users.noreply.github.com>
2023-06-21 18:41:39 +0200

    Modified SheCodes Forecast API

--- src/app.js
+++ src/app.js
@@ -21,8 +21,53 @@ function formatDate(time) {
     return `${day}   ${hours}:${minutes}`;
 }
 
+function formatDay(time) {
+  let date = new Date(time * 1000);
+  let day = date.getDay();
+  let days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+  return days[day];
+}
+
+function showForecast(response){
+  console.log(response.data.daily);
+  let daily = response.data.daily;
+  let forecast = document.querySelector("#weekly-forecast");
+
+  let forecastHTML = `<div class="row">`;
+  
+  daily.forEach(function (day,index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+          <div class="week-day" id="day">${formatDay(day.time)}
+          <img src=${day.condition.icon_url} alt=${day.condition.icon}/>
+          <span class="degrees"><strong> ${Math.round(
+            day.temperature.maximum
+          )}° </strong> <em> /${Math.round(
+          day.temperature.minimum
+        )}° </em> </span>
+         </div>
+         </div>`;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecast.innerHTML = forecastHTML;
+  
+}
+
+function forecastCall(coordinates) {
+  let apiKey = "t33bbb07a7bba4737bfcaf2ca66oea00";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showForecast);
+  console.log(apiUrl);
+}
+
+
 function displayTemp (response){
     
+    forecastCall(response.data.coordinates);
     celsiusTemp = Math.round(response.data.temperature.current);
 
     let city = document.querySelector("#city");
@@ -77,7 +122,7 @@ function search (city) {
 //let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
 //let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 let apiKey = "t33bbb07a7bba4737bfcaf2ca66oea00";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=t33bbb07a7bba4737bfcaf2ca66oea00&units=metric`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemp);   
 }
 
