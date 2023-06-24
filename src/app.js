cf3e6d57d2858e149085a1ae1f8dd2829a0f1926
azasakhegwama9 <135711777+azasakhegwama9@users.noreply.github.com>
2023-06-24 14:25:18 +0200

    Modified Weather API to be more responsive

--- src/app.js
+++ src/app.js
@@ -1,35 +1,34 @@
-function formatDate(time) {
-    let date = new Date();
-    let hours = date.getHours();
-    if (hours < 10 ) {
-        hours = `0${hours}`;
-    }
-    let minutes = date.getMinutes();
-    if (minutes < 10) {
-      minutes = `0${minutes}`;
-    }
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday"
-    ];
-    let day = days[date.getDay()];
-    return `${day}   ${hours}:${minutes}`;
+function dateFormatted(time) {
+  let date = new Date();
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let weekDays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let actualDay = weekDays[date.getDay()];
+  return `${actualDay}   ${hours}:${minutes}`;
 }
 
-function formatDay(time) {
+function dayFormatted(time) {
   let date = new Date(time * 1000);
-  let day = date.getDay();
-  let days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
-  return days[day];
+  let actualDay = date.getDay();
+  let weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return weekDays[actualDay];
 }
 
 function showForecast(response){
-  console.log(response.data.daily);
   let daily = response.data.daily;
   let forecast = document.querySelector("#weekly-forecast");
 
@@ -40,14 +39,13 @@ function showForecast(response){
       forecastHTML =
         forecastHTML +
         `<div class="col-2">
-          <div class="week-day" id="day">${formatDay(day.time)}
+          <div class="week-day" id="day">${dayFormatted(day.time)} </div>
           <img src=${day.condition.icon_url} alt=${day.condition.icon}/>
           <span class="degrees"><strong> ${Math.round(
             day.temperature.maximum
           )}° </strong> <em> /${Math.round(
           day.temperature.minimum
         )}° </em> </span>
-         </div>
          </div>`;
     }
   });
@@ -61,15 +59,13 @@ function forecastCall(coordinates) {
   let apiKey = "t33bbb07a7bba4737bfcaf2ca66oea00";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showForecast);
-  console.log(apiUrl);
 }
-
 
 function displayTemp (response){
     
     forecastCall(response.data.coordinates);
     celsiusTemp = Math.round(response.data.temperature.current);
-
+    
     let city = document.querySelector("#city");
     city.innerHTML = response.data.city;
 
@@ -89,7 +85,7 @@ function displayTemp (response){
     weather.innerHTML = response.data.condition.description;
 
     let date = document.querySelector("#date");
-    date.innerHTML = formatDate(response.data.time * 1000);
+    date.innerHTML = dateFormatted(response.data.time * 1000);
 
     let icon = document.querySelector("#icon1");
     icon.setAttribute(
@@ -97,8 +93,9 @@ function displayTemp (response){
       `${response.data.condition.icon_url}`
       );
 
-  }
 
+
+  }
 
 function linkF(event) {
   event.preventDefault();
@@ -119,8 +116,6 @@ buttonLink1.addEventListener("click", linkC);
 
 
 function search (city) {
-//let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
-//let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 let apiKey = "t33bbb07a7bba4737bfcaf2ca66oea00";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemp);   
@@ -134,7 +129,6 @@ function searchCity (event){
 }
 
 let celsiusTemp = null;
-
 let buttonClick = document.querySelector("#input-form");
 buttonClick.addEventListener("submit", searchCity);
 
